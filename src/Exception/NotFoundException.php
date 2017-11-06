1acf2e9a67dbb172cafa51199459f569e24d9c1a
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-06 15:05:17 +0100

    Import from the incubator

---
+++ src/Exception/NotFoundException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Brick\Storage\Exception;
+
+/**
+ * Exception thrown when referencing a non existent path.
+ */
+class NotFoundException extends StorageException
+{
+    /**
+     * @param string          $path
+     * @param \Exception|null $previous
+     *
+     * @return NotFoundException
+     */
+    public static function pathNotFound(string $path, \Exception $previous = null) : NotFoundException
+     {
+         return new self(sprintf('The path "%s" does not exist', $path), 0, $previous);
+     }
+}
---
+++ src/Exception/StorageException.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Brick\Storage\Exception;
+
+/**
+ * Exception thrown by classes implementing the Storage interface.
+ */
+class StorageException extends \RuntimeException
+{
+    /**
+     * @param string          $path
+     * @param \Exception|null $previous
+     *
+     * @return StorageException
+     */
+    public static function putError(string $path, \Exception $previous = null) : StorageException
+    {
+        return new self(sprintf('Could not put the given object at "%s".', $path), 0, $previous);
+    }
+
+    /**
+     * @param string          $path
+     * @param \Exception|null $previous
+     *
+     * @return StorageException
+     */
+    public static function getError(string $path, \Exception $previous = null) : StorageException
+    {
+        return new self(sprintf('Could not get the given object at "%s".', $path), 0, $previous);
+    }
+
+    /**
+     * @param string          $path
+     * @param \Exception|null $previous
+     *
+     * @return StorageException
+     */
+    public static function deleteError(string $path, \Exception $previous = null) : StorageException
+    {
+        return new self(sprintf('Could not remove the given object at "%s".', $path), 0, $previous);
+    }
+}
