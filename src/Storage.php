bf67ee795e428f616a4a9a0b497dd860b5d21bd9
Benjamin Morel <benjamin.morel@gmail.com>
2020-01-09 22:19:28 +0100

    Add docs to Storage interface

--- src/Storage.php
+++ src/Storage.php
@@ -8,38 +8,50 @@ namespace Brick\Storage;
 interface Storage
 {
     /**
-     * @param string $path
-     * @param string $data
+     * Puts an object in the storage.
+     *
+     * If the object already exists, it is overwritten.
+     *
+     * @param string $path The object path.
+     * @param string $data The object data.
      *
      * @return void
      *
-     * @throws Exception\StorageException If an unknown error occurs.
+     * @throws Exception\StorageException If an error occurs.
      */
     public function put(string $path, string $data) : void;
 
     /**
-     * @param string $path
+     * Retrieves an object from the storage.
      *
-     * @return string
+     * @param string $path The object path.
+     *
+     * @return string The object data.
      *
      * @throws Exception\NotFoundException If the path is not found.
-     * @throws Exception\StorageException  If an unknown error occurs.
+     * @throws Exception\StorageException  If an error occurs.
      */
     public function get(string $path) : string;
 
     /**
+     * Returns whether an object exists for the given path.
+     *
      * @param string $path
      *
      * @return bool
+     *
+     * @throws Exception\StorageException If an error occurs.
      */
     public function has(string $path) : bool;
 
     /**
+     * Deletes an object from the storage.
+     *
      * @param string $path
      *
      * @return void
      *
-     * @throws Exception\StorageException If an unknown error occurs.
+     * @throws Exception\StorageException If an error occurs.
      */
     public function delete(string $path) : void;
 }
